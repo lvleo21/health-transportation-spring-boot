5cd4c65468c561f60afc57bc200c5caad7178c6c
lvleo21 <verasleonardo210@gmail.com>
2021-04-07 20:15:19 -0300

    first commit

---
+++ src/main/java/com/pbd/project/HealthTransportationApplication.java
@@ -0,0 +1,13 @@
+package com.pbd.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthTransportationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthTransportationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pbd/project/HealthTransportationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbd.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthTransportationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
