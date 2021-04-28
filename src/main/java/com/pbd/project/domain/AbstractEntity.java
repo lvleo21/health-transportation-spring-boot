95a006579d8f9118d2213f5615b22f723e949200
lvleo21 <verasleonardo210@gmail.com>
2021-04-28 09:33:54 -0300

    add: AbstractEntity

---
+++ src/main/java/com/pbd/project/domain/AbstractEntity.java
@@ -0,0 +1,44 @@
+package com.pbd.project.domain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@MappedSuperclass
+public class AbstractEntity<ID extends Serializable> implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private ID id;
+
+    public ID getId() {
+        return id;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AbstractEntity<?> that = (AbstractEntity<?>) o;
+
+        return id != null ? id.equals(that.id) : that.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "id = " + id;
+    }
+
+}
